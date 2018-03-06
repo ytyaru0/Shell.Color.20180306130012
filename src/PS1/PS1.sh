afa4aa710107556623db3f110d06c1049936a1d5
ytyaru0 <ytyaru0@outlook.jp>
2018-03-06 22:11:11 +0900

    PS1.shにコメント追加

--- src/PS1/PS1.sh
+++ src/PS1/PS1.sh
@@ -1,6 +1,13 @@
-# https://qiita.com/HMMNRST/items/d8fe7b3c528f447d68a6
-# https://en.wikipedia.org/wiki/ANSI_escape_code
-
+# ----------------------------------------------------
+# 環境変数PS1をかっこよくする
+# 表示例
+#   (python venv) (chroot) user@host:cd $ 
+# 引数
+#   $1: python -venv の仮想環境名
+# 参考
+#   https://qiita.com/HMMNRST/items/d8fe7b3c528f447d68a6
+#   https://en.wikipedia.org/wiki/ANSI_escape_code
+# ----------------------------------------------------
 # Select Graphic Rendition
 function SGR () {
     echo "\[\033[$1m\]"
@@ -14,24 +21,30 @@ function Array () {
     local ret=`echo $ret | sed -e 's/;$//'`
     echo $ret
 }
-venv_name=${target:+($target) }
-#venv_name=${S_R}${target}
+venv_name=$1
+venv_name=${venv_name:+($venv_name) }
 user_name="\u"
 host_name="\h"
 work_dir="\w"
 chroot=`${debian_chroot:+($debian_chroot) }`
 
 RESET=`SGR 00` # RESET, NORMAL, OFF
-. ../echo/color_define.sh
+. ./ANSI_escape_code/color_define.sh
 COL_VENV=`SGR ${COLORS["BR"]}`
 COL_CHROOT=`SGR ${COLORS["BY"]}`
 COL_USERHOST=`SGR ${COLORS["BG"]}`
 COL_DIR=`SGR ${COLORS["BB"]}`
 # 多彩な色の設定方法↓
 #COL_DIR=`SGR $(Array ${SGR_P["Bold"]} ${COLORS["B"]})` # 併用（PARAM + Color）
-#. /tmp/work/Shell.Color.20180306130012/src/echo/color8_define.sh
-#COL_DIR=`SGR $(Array 38 5 $(GetColor 5 2 1))` # 6bit color (R,G,Bは0..5の値)
-#COL_DIR=`SGR $(Array 38 2 $(GetColor 255 255 255))` # 24bit color (R,G,Bは0..255の値)
+#. ./ANSI_escape_code/SGR_P.sh
+#. ./ANSI_escape_code/color8_define.sh
+#COL_DIR=`SGR $(SGR_P 38 5 $(GetColor 5 2 1))` # 6bit color (R,G,Bは0..5の値)
+#COL_DIR=`SGR $(SGR_P 38 2 $(GetColor 255 255 255))` # 24bit color (R,G,Bは0..255の値)
+#COL_DIR=`SGR $(Color8 ${COLORS["R"]})` # 0..7, 8..15
+#COL_DIR=`SGR $(Color8 $(GetColor 5 2 1))` # 6bit color (R,G,Bは0..5の値)
+#COL_DIR=`SGR $(Color24 $(GetColor 255 196 108))` # 24bit color (R,G,Bは0..255の値)
+#COL_DIR=`SGR $(BackgroundColor8 $(GetColor 5 2 1))` # 6bit color (R,G,Bは0..5の値)
+#COL_DIR=`SGR $(BackgroundColor24 $(GetColor 255 196 108))` # 24bit color (R,G,Bは0..255の値)
 
 PS1="${COL_VENV}${venv_name}${COL_CHROOT}${chroot}${COL_USERHOST}${user_name}@${host_name}${RESET}:${COL_DIR}${work_dir} \$${RESET} "
 
